b00f80463c5f404a6a1cde83486cd582a08ffe16
Rohith Reddy <rohithreddy2219@gmail.com>
2017-11-16 02:57:55 +0530

    Removed unecessary logs

--- cpp/parallel_level_updation.cpp
+++ cpp/parallel_level_updation.cpp
@@ -21,7 +21,6 @@ int main(int argc, char *argv[])
 	edges_sql = "SELECT id, source, target, cost FROM %s";
 	edges_sql = (boost::format(edges_sql) % edge_table).str();
 	skeleton_sql = "SELECT id, source, target, cost FROM %s WHERE promoted_level <= %s";
-	std::cout << "dbname: " << dbname << ", level: " << curr_level << std::endl;
 	connections_sql = "SELECT source, array_agg(target)::bigint[] as targets INTO connections \
 			FROM pgr_makeConnected(%s) GROUP BY source;";
 	drop_sql = "DROP TABLE IF EXISTS connections";
@@ -37,6 +36,8 @@ int main(int argc, char *argv[])
 	boost::mpi::environment env(argc, argv);
 	boost::mpi::communicator world;
 	if (world.rank() == 0) {
+
+		std::cout << "dbname: " << dbname << ", level: " << curr_level << std::endl;
 		try {
 			conn_str = "dbname = %s user = postgres password = postgres \
 			hostaddr = 127.0.0.1 port = 5432";
@@ -52,7 +53,7 @@ int main(int argc, char *argv[])
 				% edge_table % curr_level).str();
 				connections_sql = (boost::format(connections_sql) 
 				% W2.quote(skeleton_sql)).str();
-				std::cout << "connections_query: " << connections_sql  << std::endl;
+				//std::cout << "connections_query: " << connections_sql  << std::endl;
 				pqxx::result cnt = W2.exec(connections_sql.c_str());
 				num_connections = cnt.affected_rows();
 				std::cout << "affected_rows: " <<  num_connections << std::endl;
@@ -110,9 +111,9 @@ int main(int argc, char *argv[])
 					//Updating promoted level of edges	
 					pqxx::work W(C);
 					update_sql = (boost::format(update_sql) 
-					% edge_table % curr_level % W.quote(edges_sql) 
+					%edge_table %curr_level %W.quote(edges_sql) 
 					%sources[i] %targets_array[i] %curr_level).str();
-					std::cout << "update_query: " << update_sql  << std::endl;
+					//std::cout << "update_query: " << update_sql  << std::endl;
 					W.exec(update_sql.c_str());
 					W.commit();
 				}
