366bd03ca6b62b5a5620f1c52a9345c073e95244
Rohith Reddy <rohithreddy2219@gmail.com>
2017-11-16 16:38:30 +0530

    Updating edge and vertex table

--- sql/populate_betweenness.sql
+++ sql/populate_betweenness.sql
@@ -7,17 +7,24 @@ CREATE TEMP TABLE tmp (
    source            BIGINT                  NOT NULL,
    target            BIGINT                  NOT NULL,
    betweenness       DOUBLE PRECISION,
-   level             INTEGER               
+   level             INTEGER,
+   component         INTEGER[]      		DEFAULT '{}'
 );
 
-COPY tmp FROM '/home/rohithreddy/mystuff/research/parallel-betweenness/data/out_sample_data.csv' delimiter ',' csv;
+COPY tmp FROM '/home/rohithreddy/mystuff/research/pgrouting-optimisation/data/out_sample_data.csv' delimiter ',' csv;
 CREATE INDEX results_index ON tmp(id, source, target);
 
 UPDATE cleaned_ways
-SET    betweenness = tmp.betweenness, level = tmp.level, promoted_level = tmp.level
+SET    betweenness = tmp.betweenness, level = tmp.level, promoted_level = tmp.level,
+component = tmp.component::INTEGER[]
 FROM   tmp
 WHERE  cleaned_ways.id = tmp.id
 AND cleaned_ways.source = tmp.source AND cleaned_ways.target = tmp.target;
+
+UPDATE cleaned_ways_vertices_pgr
+SET component = tmp.component
+FROM tmp
+WHERE cleaned_ways_vertices_pgr.id = tmp.source;
 
 DROP TABLE tmp; -- else it is dropped at end of session automatically
 
