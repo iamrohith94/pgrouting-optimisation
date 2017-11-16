1f6cb4590646cf18ca27e39628b4b3a5cd815e60
Rohith Reddy <rohithreddy2219@gmail.com>
2017-11-16 16:38:49 +0530

    Updated to run the whole process

--- run.sh
+++ run.sh
@@ -1,10 +1,20 @@
+#Creating necessary schema
+psql -U postgres $1 < sql/preprocess.sql
+
+#Generating betweenness values
 mpic++ -o p_b cpp/parallel_betweenness.cpp -I/usr/local/include/ -lboost_graph_parallel -lboost_mpi -lboost_system -lboost_serialization
-#g++ -o s_b cpp/sequential_betweenness.cpp -I/usr/local/include/ -lboost_graph  -lboost_system 
-#./s_b $1
 mpirun -n $2 ./p_b $1
-#sudo su postgres
-#psql $1 -v name=\'$1\'
-#psql $1 < preprocess.sql
-#psql $1 < populate_levels.sql
-#psql $1 < populate_components.sql 
-#exit
+
+#Updating the db with betweenness values
+psql -U postgres $1 < sql/populate_betweenness.sql
+
+#Updating the db with level values
+mpic++ -o p_l cpp/parallel_level_updation.cpp -I/usr/local/include/ -lpq -lpqxx -lboost_graph_parallel -lboost_mpi -lboost_system -lboost_serialization
+for i in `seq 1 10`;
+do
+	mpirun -n $2 ./p_l $1 $i
+done
+
+#Updating the db with component values
+mpic++ -o p_c cpp/parallel_comp_updation.cpp -I/usr/local/include/ -lpq -lpqxx -lboost_graph_parallel -lboost_mpi -lboost_system -lboost_serialization
+mpirun -n $2 ./p_c $1
