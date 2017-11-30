a2a049249940305a0afb043946150bd70a98de96
Rohith Reddy <rohithreddy2219@gmail.com>
2017-11-30 17:02:48 +0530

    Added performance query

--- sql/performance.sql
+++ sql/performance.sql
@@ -4,7 +4,6 @@ CREATE OR REPLACE FUNCTION performanceAnalysis(
     edge_table TEXT,
     vertex_table TEXT,
     num_pairs INTEGER,
-    skeleton_level INTEGER,
     num_levels INTEGER DEFAULT 10,
     num_iterations INTEGER DEFAULT 1,
     directed BOOLEAN DEFAULT true,
@@ -23,6 +22,7 @@ $body$
 DECLARE
   source_comp INTEGER;
   target_comp INTEGER;
+  level INTEGER;
   source_sql TEXT;
   target_sql TEXT;
   final_sql TEXT;
@@ -45,34 +45,39 @@ BEGIN
     */
     EXECUTE 'SELECT ARRAY(SELECT id FROM '|| vertex_table ||' ORDER BY RANDOM() LIMIT '||num_pairs||')' INTO start_vids;
     EXECUTE 'SELECT ARRAY(SELECT id FROM '|| vertex_table ||' ORDER BY RANDOM() LIMIT '||num_pairs||')' INTO end_vids;
-    IF level = num_levels THEN
-        final_sql = 'SELECT id, source, target, cost FROM ' || edge_table;
-        RETURN QUERY SELECT a.source, a.target, skeleton_level, a.num_edges, a.num_vertices,
-            a.graph_build_time, a.avg_computation_time, a.path_len 
-            FROM pgr_performanceAnalysis(final_sql, 'pgr_dijkstra', start_vids, end_vids, num_iterations, directed) AS a;
-    ELSE 
-        FOR i in 1 .. array_upper(start_vids, 1)
-        LOOP
-            source := start_vids[i];
-            target := end_vids[i];
-            RAISE NOTICE 'source %', source;
-            RAISE NOTICE 'target %', target;
-            source_sql := 'SELECT component['|| skeleton_level ||'] FROM ' 
-            || vertex_table 
-            || ' WHERE id = ' || source;
-            target_sql := 'SELECT component['|| skeleton_level ||'] FROM ' 
-            || vertex_table 
-            || ' WHERE id = ' || target;
-            EXECUTE source_sql INTO source_comp;
-            EXECUTE target_sql INTO target_comp;
+    
+    FOR level in 1 .. num_levels
+    LOOP
+        RAISE NOTICE 'level %', level;
+        IF level = num_levels THEN
+            final_sql = 'SELECT id, source, target, cost FROM ' || edge_table;
+            RETURN QUERY SELECT a.source, a.target, level, a.num_edges, a.num_vertices,
+                a.graph_build_time, a.avg_computation_time, a.path_len 
+                FROM (SELECT * FROM pgr_performanceAnalysis(final_sql, 'pgr_dijkstra', start_vids, end_vids, num_iterations, directed)) AS a;
+        ELSE 
+            FOR i in 1 .. array_upper(start_vids, 1)
+            LOOP
+                source := start_vids[i];
+                target := end_vids[i];
+                --RAISE NOTICE 'source %', source;
+                --RAISE NOTICE 'target %', target;
+                source_sql := 'SELECT component_'|| level ||' FROM ' 
+                || vertex_table 
+                || ' WHERE id = ' || source;
+                target_sql := 'SELECT component_'|| level ||' FROM ' 
+                || vertex_table 
+                || ' WHERE id = ' || target;
+                EXECUTE source_sql INTO source_comp;
+                EXECUTE target_sql INTO target_comp;
 
-            final_sql := 'SELECT id, source, target, cost FROM '
-            || edge_table || ' WHERE promoted_level['|| skeleton_level ||'] = 0 OR component['|| skeleton_level ||'] = ' 
-            || source_comp || ' OR component['|| skeleton_level ||'] = ' || target_comp;
-            RETURN QUERY SELECT a.source, a.target, skeleton_level, a.num_edges, a.num_vertices,
-            a.graph_build_time, a.avg_computation_time, a.path_len 
-            FROM pgr_performanceAnalysis(final_sql, 'pgr_dijkstra'::TEXT, ARRAY[source], ARRAY[target], num_iterations, directed) AS a;
-        END LOOP;
-    END IF;
+                final_sql := 'SELECT id, source, target, cost FROM '
+                || edge_table || ' WHERE promoted_level <= '|| level ||' OR component_'|| level ||' = ' 
+                || source_comp || ' OR component_'|| level ||' = ' || target_comp;
+                RETURN QUERY SELECT a.source, a.target, level, a.num_edges, a.num_vertices,
+                a.graph_build_time, a.avg_computation_time, a.path_len 
+                FROM (SELECT * FROM pgr_performanceAnalysis(final_sql, 'pgr_dijkstra'::TEXT, ARRAY[source]::BIGINT[], ARRAY[target]::BIGINT[], num_iterations, directed)) AS a;
+            END LOOP;
+        END IF;
+    END LOOP;
 END
 $body$ language plpgsql volatile;
